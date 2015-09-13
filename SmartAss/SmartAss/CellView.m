ea7c5a9940bf80b3d4c42a240b0d91b8a19c6a90
Pierre Karashchuk <pierre@Pierres-MacBook-Pro.local>
2015-09-12 20:02:03 -0700

    add labels and rounded corners

--- SmartAss/SmartAss/CellView.m
+++ SmartAss/SmartAss/CellView.m
@@ -7,6 +7,8 @@
 //
 
 #import "CellView.h"
+#import <QuartzCore/QuartzCore.h>
+
 
 @implementation CellView
 
@@ -16,42 +18,21 @@
     
     if (self)
     {
-        
+        self.layer.cornerRadius = 10;
+        self.layer.masksToBounds = YES;
     }
     
     return self;
 }
 
-//- (void) drawRect: (CGRect) rect {
-//    [super drawRect:rect];
-////    self.backgroundColor = [UIColor whiteColor];
-////    CGRect rectangle = rect;
-////    CGContextRef context = UIGraphicsGetCurrentContext();
-////    CGContextSetRGBFillColor(context, 1.0, 1.0, 1.0, 1.0);
-////    CGContextFillRect(context, rectangle);
-//}
-
 - (void) updatePressure: (NSNumber *) percentage {
-    UIColor *pressureColor = [UIColor colorWithRed:1.0
-    green:0.0
-    blue:0.0
-    alpha:(float)[percentage floatValue]/100];
-//    NSLog(@"%f",[percentage floatValue]/100);
+    float alpha = [percentage floatValue]/100;
+    UIColor *pressureColor = [UIColor colorWithRed:1.0*alpha
+                                             green:1.0*(1-alpha)
+                                              blue:0.0
+                                             alpha:1.0];
     self.backgroundColor = pressureColor;
     [self setNeedsDisplay];
-    [self.superview setNeedsDisplay];
-    
 }
-
-//- (void)drawRect:(CGRect)rect {
-//    [super drawRect:rect];
-//    CGContextRef context = UIGraphicsGetCurrentContext();
-//    CGPathRef path = CGPathCreateWithRect(rect, NULL);
-//    CGContextSetRGBFillColor(context, 1.0, 0.0, 0.0, 1.0);
-//    CGContextSetRGBStrokeColor(context, 0.0, 1.0, 2.0, 1.0);
-//    CGContextAddPath(context, path);
-//    CGContextDrawPath(context, kCGPathFillStroke);
-//    CGPathRelease(path);
-//}
 
 @end
--- SmartAss/SmartAss/ViewController.m
+++ SmartAss/SmartAss/ViewController.m
@@ -9,6 +9,7 @@
 #import "ViewController.h"
 #import "AFNetworking.h"
 
+
 @interface ViewController ()
 
 @end
@@ -22,56 +23,95 @@
     [self setUI];
     
     [NSTimer scheduledTimerWithTimeInterval:.25
-    target:self
-    selector:@selector(updatePressure)
-    userInfo:nil
-    repeats:YES];
+                                     target:self
+                                   selector:@selector(updatePressure)
+                                   userInfo:nil
+                                    repeats:YES];
 }
 
 - (void) setUI {
-    self.grid = [[UIView alloc] initWithFrame:
-        CGRectMake(
-            0,
-            (self.view.frame.size.height - self.view.frame.size.width)/2,
-            self.view.frame.size.width,
-            self.view.frame.size.width
-        )
-    ];
+    int space_between = 15;
+    int label_height = 30;
+
+
     
-    [self.grid setBackgroundColor: [UIColor blackColor]];
+    self.grid = [[UIView alloc] initWithFrame:
+                 CGRectMake(
+                            0,
+                            (self.view.frame.size.height - self.view.frame.size.width)/2,
+                            self.view.frame.size.width,
+                            self.view.frame.size.width
+                            )
+                 ];
+    
+    int height =  (self.grid.frame.size.height-space_between*2)/2;
+    int width = (self.grid.frame.size.width-space_between*3)/2;
+    UILabel *label;
+    
+    [self.grid setBackgroundColor: [UIColor whiteColor]];
     
     self.cell1 = [[CellView alloc] initWithFrame:
-        CGRectMake(
-            0,
-            self.view.frame.origin.y,
-            self.grid.frame.size.width/2,
-            self.grid.frame.size.height/2
-        )
-    ];
+                  CGRectMake(
+                             space_between,
+                             self.view.frame.origin.y,
+                             width,
+                             height
+                             )
+                  ];
+    
+    
+    label = [[UILabel alloc] initWithFrame:CGRectMake(self.cell1.frame.origin.x, self.cell1.frame.origin.y - label_height, width, label_height)];
+    label.text = @"Front-left";
+    label.textAlignment = NSTextAlignmentCenter;
+    label.textColor = [UIColor grayColor];
+    [self.grid addSubview:label];
+    
     self.cell2 = [[CellView alloc] initWithFrame:
-         CGRectMake(
-            self.view.frame.size.width/2,
-            self.view.frame.origin.y,
-            self.grid.frame.size.width/2,
-            self.grid.frame.size.height/2
-        )
-    ];
+                  CGRectMake(
+                             space_between*2 + (self.grid.frame.size.width-space_between*3)/2,
+                             self.view.frame.origin.y,
+                             width,
+                             height
+                             )
+                  ];
+    
+    label = [[UILabel alloc] initWithFrame:CGRectMake(self.cell2.frame.origin.x, self.cell2.frame.origin.y - label_height, width, label_height)];
+    label.text = @"Front-right";
+    label.textAlignment = NSTextAlignmentCenter;
+    label.textColor = [UIColor grayColor];
+    [self.grid addSubview:label];
+    
     self.cell3 = [[CellView alloc] initWithFrame:
-        CGRectMake(
-            0,
-            self.view.frame.origin.y + self.grid.frame.size.height/2,
-            self.grid.frame.size.width/2,
-            self.grid.frame.size.height/2
-        )
-    ];
+                  CGRectMake(
+                             space_between,
+                             self.view.frame.origin.y + self.grid.frame.size.height/2,
+                             width,
+                             height
+                             )
+                  ];
+    
+    label = [[UILabel alloc] initWithFrame:CGRectMake(self.cell3.frame.origin.x, self.cell3.frame.origin.y + height, width, label_height)];
+    label.text = @"Bottom-left";
+    label.textAlignment = NSTextAlignmentCenter;
+    label.textColor = [UIColor grayColor];
+    [self.grid addSubview:label];
+    
+    
     self.cell4 = [[CellView alloc] initWithFrame:
-        CGRectMake(
-            self.view.frame.size.width/2,
-            self.view.frame.origin.y + self.grid.frame.size.height/2,
-            self.grid.frame.size.width/2,
-            self.grid.frame.size.height/2
-        )
-    ];
+                  CGRectMake(
+                             space_between*2 + (self.grid.frame.size.width-space_between*3)/2,
+                             self.view.frame.origin.y + self.grid.frame.size.height/2,
+                             width, height
+                             )
+                  ];
+    
+    label = [[UILabel alloc] initWithFrame:CGRectMake(self.cell4.frame.origin.x, self.cell4.frame.origin.y + height, width, label_height)];
+    label.text = @"Bottom-right";
+    label.textAlignment = NSTextAlignmentCenter;
+    label.textColor = [UIColor grayColor];
+    [self.grid addSubview:label];
+    
+    
     [self.grid addSubview: self.cell1];
     [self.grid addSubview: self.cell2];
     [self.grid addSubview: self.cell3];
@@ -87,22 +127,19 @@
     [manager GET:@"/getPressure" parameters:nil success:^(AFHTTPRequestOperation *operation, id responseObject) {
         NSError* error;
         NSDictionary* pressureDict = [NSJSONSerialization JSONObjectWithData:responseObject
-            options:kNilOptions
-            error:&error
-        ];
+                                                                     options:kNilOptions
+                                                                       error:&error
+                                      ];
         NSNumber *flex1 = [pressureDict objectForKey: @"flex1"];
         NSNumber *flex2 = [pressureDict objectForKey: @"flex2"];
         NSNumber *flex3 = [pressureDict objectForKey: @"flex3"];
         NSNumber *flex4 = [pressureDict objectForKey: @"flex4"];
-//        NSLog(@"%@", flex1);
         
         [self.cell1 updatePressure: flex1];
-//        [self.cell1 removeFromSuperview];
-//        [self.grid addSubview: self.cell1];
         [self.cell2 updatePressure: flex2];
         [self.cell3 updatePressure: flex3];
         [self.cell4 updatePressure: flex4];
-        [self.grid setNeedsDisplay];
+//        [self.grid setNeedsDisplay];
     } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
         NSLog(@"Error: %@", error);
     }];
